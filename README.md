6af3582c97f36733cb0a57a31ff63d34e0153cd7
Sarah Wrzos <swrzos@ithaca.edu>
2026-02-07 19:59:19 -0500

    restructured code, added classes

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 # npm install @svgdotjs/svg.js
 
 # Ctrl+shift+i console
+
+# to run locally: (write in terminal) npm run dev
+
+to do:
+create functions
+    reset all
+    delete line
+    add diameter through point
+
+
+figure out how to
+    move the points and generate new line (show a list, manually change numbers or drag point)
+    calculate angles
+    only select points inside of circle
+    only show inside of circle
+    
--- geometryapp/src/routes/+page.svelte
+++ geometryapp/src/routes/+page.svelte
@@ -2,72 +2,88 @@
 <script>
   import { onMount } from 'svelte'
   import { SVG } from '@svgdotjs/svg.js'
-  import { createLineTool } from '$lib/tools/lineTool'
-  import { createhLineTool } from '$lib/tools/hlineTool'
 
-  import { circleCenter } from '$lib/circleStore.js'
+	import { Shapes } from '$lib/geometry/Shapes';
 
   let container
   let draw
   let activeTool = null
+  let shapes
+
+  // Function to get x,y coordinates of click relative to SVG
+  function getClickCoords(event) {
+    const pt = draw.node.createSVGPoint()
+    pt.x = event.clientX
+    pt.y = event.clientY
+    return pt.matrixTransform(draw.node.getScreenCTM().inverse())
+  }
 
   onMount(() => {
     draw = SVG().addTo(container).size(container.clientWidth, container.clientHeight)
 
-    // create unit circle
+    // create unit circle custom to window
     const centerX = container.clientWidth / 2
     const centerY = container.clientHeight / 2
     const diameter = container.clientHeight / 2
     const radius = diameter / 2
 
-    draw.circle(diameter) // diameter
+    draw.circle(diameter)
         .center(centerX, centerY)
         .fill('none')
         .stroke({ width: 2, color: '#000' })
 
-    circleCenter.set({ x: centerX, y: centerY , r: radius})
+    shapes = new Shapes(centerX, centerY, radius)
+    let tempPoint = null
 
-    draw.on("click", (e) => {
-      console.log("active tool:", activeTool)
+    draw.on("click", (event) => {
+      const coords = getClickCoords(event);
+      const point = shapes.addPoint(coords.x, coords.y);
+
+      if (tempPoint === null) {
+        tempPoint = point; // store the Point object
+      } else {
+        shapes.addLine(tempPoint, point); // pass Point objects
+        tempPoint = null;
+      }
+
+      // Draw all points and lines
+      draw.clear();
+      draw.circle(diameter)
+          .center(centerX, centerY)
+          .fill('none')
+          .stroke({ width: 2, color: '#000' });
+
+      shapes.drawAll(draw);
+
+      console.log(coords.x, coords.y); 
+      console.log(shapes.lines)
+    });
+
+
+
+  //   draw.on("click", (e) => {
+  //     console.log("active tool:", activeTool)
       
-      if (!activeTool) return
-      const pt = draw.node.createSVGPoint()
-      pt.x = e.clientX
-      pt.y = e.clientY
-      const svgCoords = pt.matrixTransform(
-        draw.node.getScreenCTM().inverse()
-      )
-      activeTool.onClick(svgCoords)
-    })
+  //     if (!activeTool) return
+  //     const pt = draw.node.createSVGPoint()
+  //     pt.x = e.clientX
+  //     pt.y = e.clientY
+  //     const svgCoords = pt.matrixTransform(
+  //       draw.node.getScreenCTM().inverse()
+  //     )
+  //     activeTool.onClick(svgCoords)
+  //   })
 
-    window.addEventListener('resize', updateSize)
+  //   window.addEventListener('resize', updateSize)
 
-    return () => window.removeEventListener('resize', updateSize)
+  //   return () => window.removeEventListener('resize', updateSize)
   })
 
-  function useLineTool() {
-    activeTool?.reset()
-    activeTool = createLineTool(draw)
-  }
-
-  function usehLineTool(){
-    activeTool?.reset()
-    activeTool = createhLineTool(draw)
-  }
-
-  function updateSize() {
-    if (draw && container) draw.size(container.clientWidth, container.clientHeight)
-  }
+  // function updateSize() {
+  //   if (draw && container) draw.size(container.clientWidth, container.clientHeight)
+  // }
 
 </script>
-
-<button on:click={useLineTool}>
-  Draw Line
-</button>
-
-<button on:click={usehLineTool}>
-  Draw Hyperbolic Line
-</button>
 
 <div bind:this={container} style="width: 100vw; height: 100vh;"></div>
 
